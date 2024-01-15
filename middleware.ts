f523acf7a13f9f62afe127026b0866c156e489ca
Dhanubalde <kedanubalde41@gmail.com>
2024-01-15 09:46:52 +0800

    relaod all auth funstions

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { auth } from "./auth"
 
 export default auth((req) => {
-  // req.auth
+  const isLoggedIn = !!req.auth
+  console.log("Route: ", req.nextUrl.pathname);
+  console.log("isLoggedin: ",isLoggedIn);
+  
 })
 
 // Optionally, don't invoke Middleware on some paths
