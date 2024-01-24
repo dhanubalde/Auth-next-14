95bcb31c22839ea38e80d20309fff9297f2b796c
Dhanubalde <kedanubalde41@gmail.com>
2024-01-24 17:28:13 +0800

    admin section

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,8 +1,35 @@
 "use client"
 
-import { Card, CardHeader } from "@/components/ui/card"
+import { admin } from "@/actions/admin"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { toast } from "sonner"
 
 const AdminPage = () => {
+
+  const onServerActionClick = () => { 
+    admin()
+      .then((data)=> { 
+        if (data.error) { 
+          toast.error(data.error)
+        }
+
+        if (data.success) { 
+          toast.success(data.success)
+        }
+      })
+  }
+
+  const onApiRouteClick = () => { 
+    fetch("/api/admin")
+      .then((response) => { 
+        if (response.ok) {
+          toast.success("Allowed API Route!");
+        } else { 
+          toast.error("Forbidden API Route!");
+        }
+      })
+  }
+
   return (
     <div>
 
@@ -12,6 +39,9 @@ const AdminPage = () => {
             Admin
           </p>
         </CardHeader>
+        <CardContent>
+          
+        </CardContent>
       </Card>
     </div>
   )
